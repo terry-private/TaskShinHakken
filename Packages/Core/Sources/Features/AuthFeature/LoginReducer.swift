7d5f2840b969feb60b459627a4421252efc77ff9
terry <ataeteruhito@gmail.com>
2025-06-09 23:25:44 +0900

    #5 LoginView作成

---
+++ Packages/Core/Sources/Features/AuthFeature/LoginReducer.swift
@@ -0,0 +1,45 @@
+import ComposableArchitecture
+import CoreClient
+import Entity
+
+@Reducer
+public struct LoginReducer: Sendable {
+    @ObservableState
+    public struct State: Equatable {
+        var email: String = ""
+        var password: String = ""
+        var logining: Bool = false
+
+        public init() {}
+    }
+
+    public enum Action: BindableAction {
+        case binding(BindingAction<State>)
+        case onTapLoginButton
+        case loginSucceeded(User.ID)
+    }
+
+    @Dependency(\.loginClient) var loginClient
+
+    public init() {}
+
+    public var body: some ReducerOf<Self> {
+        BindingReducer()
+        Reduce { state, action in
+            switch action {
+            case .binding:
+                return .none
+            case .onTapLoginButton:
+                state.logining = true
+                return .run { send in
+                    if let userID = try? await self.loginClient.login() {
+                        await send(.loginSucceeded(userID))
+                    }
+                }
+            case .loginSucceeded:
+                state.logining = false
+                return .none
+            }
+        }
+    }
+}
---
+++ Packages/Core/Sources/Features/AuthFeature/LoginView.swift
@@ -0,0 +1,124 @@
+import ComposableArchitecture
+import Entity
+import SwiftUI
+
+public struct LoginView: View {
+    @Bindable var store: StoreOf<LoginReducer>
+
+    public init(store: StoreOf<LoginReducer>) {
+        self.store = store
+    }
+
+    public var body: some View {
+        VStack {
+            VStack {
+                header
+                    .padding(.bottom)
+
+                CustomTextfield(placeholder: "メールアドレス", text: $store.email)
+                CustomTextfield(placeholder: "パスワード",text: $store.password, isSecret: true)
+
+                HStack {
+                    Spacer()
+                    Button(action: {}) {
+                        Text("Forgot Password?")
+                    }
+                }
+                .padding(.trailing, 24)
+
+                loginButton
+                    .padding()
+
+
+                Text("or")
+                    .padding()
+
+                Button("Google Login!!") {
+                    // Call Google Login Logic
+                }
+            }
+            .padding(.top, 52)
+            Spacer()
+
+            Button("新規アカウント作成") {
+
+            }
+            .padding()
+        }
+    }
+}
+
+extension LoginView {
+    var header: some View {
+        VStack {
+            Text("ログイン")
+                .font(.largeTitle)
+                .fontWeight(.medium)
+                .padding()
+
+            Text("メールアドレスとパスワードでログイン")
+                .multilineTextAlignment(.center)
+        }
+    }
+    var loginButton: some View {
+        Button {
+            store.send(.onTapLoginButton)
+        } label: {
+            HStack {
+                Spacer()
+                if store.logining {
+                    ProgressView()
+                } else {
+                    Text("Login")
+                        .foregroundColor(.white)
+                }
+                Spacer()
+            }
+        }
+        .disabled(store.logining)
+        .padding()
+        .background(.orange)
+        .cornerRadius(12)
+    }
+}
+
+struct CustomTextfield: View {
+    @Binding var text: String
+    var placeholder: String
+    var isSecret: Bool
+    init(placeholder: String = "", text: Binding<String>, isSecret: Bool = false) {
+        self.placeholder = placeholder
+        self._text = text
+        self.isSecret = isSecret
+    }
+
+    @ViewBuilder
+    var textField: some View {
+        if isSecret {
+            SecureField(placeholder, text: $text)
+        } else {
+            TextField(placeholder, text: $text)
+        }
+    }
+
+    var body: some View {
+        textField
+            .padding(16)
+            .overlay(
+                RoundedRectangle(cornerRadius: 6)
+                    .stroke()
+            )
+            .padding(.horizontal, 24)
+            .padding(.vertical, 12)
+    }
+}
+#Preview {
+    LoginView(
+        store: .init(
+            initialState: .init(),
+            reducer: {
+                LoginReducer()
+            }
+        )
+    )
+}
